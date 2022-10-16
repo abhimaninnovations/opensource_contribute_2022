afa228d05b89c34691b805228ac1f415245672bc
Rakesh <rakesh@Rakeshs-MacBook-Pro.local>
2022-10-16 15:25:55 +0530

    bubble sort

---
+++ main.js
@@ -0,0 +1,16 @@
+let bubbleSort = (inputArr) => {
+    let len = inputArr.length;
+    let checked;
+    do {
+        checked = false;
+        for (let i = 0; i < len; i++) {
+            if (inputArr[i] > inputArr[i + 1]) {
+                let tmp = inputArr[i];
+                inputArr[i] = inputArr[i + 1];
+                inputArr[i + 1] = tmp;
+                checked = true;
+            }
+        }
+    } while (checked);
+    return inputArr;
+};
